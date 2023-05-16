1e4afce948d5e8f79edbc990a250f7e01a8f09ce
kihyeoon <bangion93@gmail.com>
2023-05-16 15:38:37 +0900

    💄 Posts 카테고리 글씨 강조 효과 추가

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -6,13 +6,17 @@ interface Props {
 
 export default function Categories({ categories, selected, onClick }: Props) {
   return (
-    <nav className=" ml-3">
-      <h2 className="font-bold underline">Category</h2>
+    <nav className="ml-3">
+      <h2 className="mb-2 border-b-2 border-blue-400 text-lg font-bold">
+        Category
+      </h2>
       <ul>
         {categories.map((category) => (
           <li
             key={category}
-            className="cursor-pointer hover:underline"
+            className={`cursor-pointer hover:text-blue-500 hover:underline ${
+              selected === category && "font-bold text-blue-500"
+            }`}
             onClick={() => onClick(category)}
           >
             {category}
