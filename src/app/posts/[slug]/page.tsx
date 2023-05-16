361ff438fae393dccf5437b010f5258bdad5ab6a
kihyeoon <bangion93@gmail.com>
2023-05-16 17:09:26 +0900

    💄 PostDetailPage UI 개선

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import MarkdownViewer from "@/components/MarkdownViewer";
 import { getContent as getPostContent, getPost } from "@/service/posts";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import { AiTwotoneCalendar } from "react-icons/ai";
 
 interface Props {
   params: {
@@ -11,15 +13,31 @@ interface Props {
 export default async function PostDetailPage({ params: { slug } }: Props) {
   const post = await getPost(slug);
   const content = await getPostContent(slug);
-
   if (!post) {
     notFound();
   }
 
+  const { title, path, date, description } = post;
+
   return (
-    <>
-      <h2>{post.title}</h2>
-      <MarkdownViewer content={content} />
-    </>
+    <article className="overflow-hidden rounded-xl bg-gray-100 shadow-lg">
+      <Image
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+        className="max-h-[500px] w-full"
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center gap-1 self-end">
+          <AiTwotoneCalendar className="text-blue-400" />
+          <p className="font-semibold">{date}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="mb-8 mt-4 w-44 border-2 border-blue-500" />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
