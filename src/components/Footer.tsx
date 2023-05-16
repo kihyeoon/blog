67825843ccc2fc30c3be335178a5e9dabeb44285
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-05-16 19:51:28 +0900

    Revert "Home, Posts, PostDetail 페이지 구현 (#1)"
    This reverts commit 68a573da9bd0088cbaa0fd26377494cb83a62748.

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-slate-900 py-2 text-center text-sm text-white">
-      ki hyeon | All Right Reserved.
-    </footer>
-  );
-}
